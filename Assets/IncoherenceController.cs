fd18d023f569060c8f0bb258f052c4c9c9ee1ccb
Unknown <dcarr1@gmail.com>
2017-01-31 16:20:01 -0500

    Trying to do stuff.

---
+++ Assets/IncoherenceController.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class IncoherenceController : MonoBehaviour {
+
+	// 'Incoherence' is expressed in various ways depending on the inherited. Ranges from 0 to 1.
+	public float incoherence = 0.0f;
+
+	// How often incoherence is expressed (seconds)
+	public float interval = 1f;
+
+	// Seconds since last interval (used as a timer)
+	float elapsedSeconds = 0.0f;
+
+	void Update()
+	{
+		Debug.Log("Doing It");
+
+		// See if it is time to express incoherence
+		elapsedSeconds += Time.deltaTime;
+
+		if (elapsedSeconds > interval) {
+			BroadcastMessage("ExpressIncoherence", incoherence);
+			elapsedSeconds = 0.0f;
+		}
+	}
+
+	void IncreaseIncoherence(float increaseAmt) {
+		incoherence += increaseAmt;
+	}
+}
