3181865925b640e6ddd3f11a573b8e7e2c69a23a
Jenna <jenna.galka@gmail.com>
2017-02-02 13:30:52 -0500

    fixed a script, barely
    lets hope this works

--- Assets/Jenna/SceneLoaderScript.cs
+++ Assets/Jenna/SceneLoaderScript.cs
@@ -4,21 +4,21 @@ using System.Collections;
 
 public class SceneLoaderScript : MonoBehaviour {
 
-//	Ray ray;
-//	RaycastHit hit;
+	//	Ray ray;
+	//	RaycastHit hit;
 
 	// Use this for initialization
 	void Start () {
 		string[] sceneArray = {"Main Scene", "Blue", "Green", "Red"};
 		string sceneToRun = sceneArray[Random.Range(0, sceneArray.Length-1)];
-	
+
 	}
-	
+
 	// Update is called once per frame
 	void Update () {
-	
+
 		//if (Input.GetMouseButtonDown) {
-			//load
+		//load
 
 		//right now I'm just going to see if it loads a random scene when space is pressed
 		if (Input.GetKey (KeyCode.Space)){
