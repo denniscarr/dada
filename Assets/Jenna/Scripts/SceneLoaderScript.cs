1355156dc84082121c718f745431121d7c3add37
Jenna <jenna.galka@gmail.com>
2017-02-02 15:50:26 -0500

    a tiny change on a script
    let's hope for the best, since I'm branched!

---
+++ Assets/Jenna/Scripts/SceneLoaderScript.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class SceneLoaderScript : MonoBehaviour {
+
+	//public bool clickLoader;
+
+	public string[] levels;
+	public string sceneToRun;
+
+	public string[] questText;
+
+	// Use this for initialization
+	void Start () {
+		string[] levels = {"QuestSceneTest", "Blue", "Green", "Red"};
+		sceneToRun = levels[Random.Range(0, (levels.Length -1 ) )];
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+		Ray ray = Camera.main.ScreenPointToRay (Input.mousePosition);
+		RaycastHit hit;
+		transform.position = ray.origin + ray.direction * 2f;
+		if (Physics.Raycast (ray, out hit)) {
+
+			if (hit.collider.name.Contains ("Blue")) {
+
+				//if (Input.GetKey (KeyCode.Space)){
+				SceneManager.LoadScene (sceneToRun);
+			}
+		}
+	}
+
+	//void onOverlapEnter
+}
