1155e58b189d7d0159b0e908805de2426091e29c
Unknown <dcarr1@gmail.com>
2017-02-03 20:50:48 -0500

    Began work on randomization system.

---
+++ Assets/Dennis/Scripts/Randomizer.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Randomizer : MonoBehaviour {
+
+	// A reference to this randomizer's controller.
+	public RandomizerController myController;
+
+	void Start() {
+
+		/* FIND MY CONTROLLER */
+
+		myController = MiscFunctions.FindGameObjectInRoot(transform.root, "Randomizer Controller").GetComponent<RandomizerController>();
+	}
+}
