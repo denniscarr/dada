326dd73863b9d0410e5afa8e6353c4c6c7f6e922
Swing <swingliu0831@foxmail.com>
2017-02-07 14:11:40 -0500

    2

--- Assets/Vanilla/Scripts/ViewController.cs
+++ Assets/Vanilla/Scripts/ViewController.cs
@@ -4,18 +4,37 @@ using System.Collections;
 public class ViewController : MonoBehaviour {
 	public float speed = 10.0F;
 	public float rotationSpeed = 100.0F;
-	//float _translation = 0f;
+	bool isStop = false;
+	float _translation = 0f;
 	void Update() {
+
+
 		float translation = Input.GetAxis("Vertical") * speed;
 		float rotation = Input.GetAxis("Horizontal") * rotationSpeed;
 		translation *= Time.deltaTime;
 		rotation *= Time.deltaTime;
-		//transform.Translate(-translation*transform.forward);
 		transform.Translate(0, 0, -translation);
 		transform.Rotate(0, rotation, 0);
-		//_translation = translation;
+		_translation = translation;
 	}
 
+	void OnCollisionEnter(Collision other){
+		Debug.Log(other.collider.name+" enter");
+		if(other.collider.name.Contains("Cube")){
+
+			//isStop = true;
+		}
+
+	}
+
+	void OnCollisionExit(Collision other){
+		Debug.Log(other.collider.name+" exit");
+		if(other.collider.name.Contains("Cube")){
+
+			//isStop = false;
+		}
+
+	}
 //	void OnTriggerEnter(Collider other) {
 //		Debug.Log(other.name);
 //		transform.Translate(0, 0, _translation);
