2e24f4ffa209b84b4386cbaff9f2718a7104d64c
Unknown <dcarr1@gmail.com>
2017-02-08 21:43:03 -0500

    saved scale implemented

--- Assets/Prototype/Scripts/InteractionSettings.cs
+++ Assets/Prototype/Scripts/InteractionSettings.cs
@@ -6,4 +6,13 @@ public class InteractionSettings : MonoBehaviour {
 
 	public bool ableToBeCarried;	// Whether the object is able to be carried.
 	public bool usable;	// Whether the object is usable.
+
+	Vector3 savedScale;
+	Vector3 SavedScale {
+		get;
+	}
+
+	void Start() {
+		savedScale = transform.localScale;
+	}
 }
