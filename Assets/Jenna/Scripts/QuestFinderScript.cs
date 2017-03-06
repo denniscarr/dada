0a83cf36a2f6392d6921bb4e56141a3256c39154
Jenna <jenna.galka@gmail.com>
2017-03-06 18:14:25 -0500

    small script changes for when i pull

--- Assets/Jenna/Scripts/QuestFinderScript.cs
+++ Assets/Jenna/Scripts/QuestFinderScript.cs
@@ -26,9 +26,8 @@ public class QuestFinderScript : MonoBehaviour {
 
 		// loop through the items in the scene
 		for (int i = 0; i < objectsSize; i++) {
-
 			InteractionSettings iset = objects [i].GetComponentInChildren<InteractionSettings> ();
-			if (iset != null) {
+				if (iset != null) {
 				interactables.Add (iset.gameObject);
 
 				// all right, so if it's on the list, calculate a random float
@@ -42,6 +41,25 @@ public class QuestFinderScript : MonoBehaviour {
 					}
 				}
 			}
+		
+			foreach (GameObject go in interactables){
+
+				if (iset.canBeUsedForQuests == true) {
+					Debug.Log (go + "can be used for quests");
+					// I think the generator will have to be a get/set maybe, or a function,
+					// not really sure, but it'll go here
+					// it's hard because, like, the iset had to be nested in the for loop because reasons
+					// so it'll be hard to access outside of this function.
+					// I guess it'll all go in start for now, it's not like it can go anywhere else
+					// might make load times kind of slow, but it'll be done before the player is there
+					// what will be difficult, however, is making it reactive when new objects are added
+					// but that's something I/we can handle later.
+				}
+			}
 		}
+	}
+
+	public void Generator(GameObject quester, QuestObject quo, int questID){
+		
 	}
 }
