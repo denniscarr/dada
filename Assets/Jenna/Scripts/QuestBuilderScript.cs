c3cac6eff507e62b2dd387e534e28926d5e58fb5
Jenna <jenna.galka@gmail.com>
2017-03-06 23:13:27 -0500

    streamreader/writer for verbs and nouns
    as well as: adding questable items to a list, adding the questobject script to those items...next is generating quests from the verb/noun lists and then adding them to questmanager

---
+++ Assets/Jenna/Scripts/QuestBuilderScript.cs
@@ -0,0 +1,89 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+
+// ******* I AM NOT CERTAIN IF THIS SHOULD GO IN THE QUEST MANAGER, OR IF IT SHOULD BE ADDED
+// TO QUESTABLE OBJECTS VIA THE QUESTFINDER SCRIPT **********
+
+public class QuestBuilderScript : MonoBehaviour {
+
+	// quest finder
+	public QuestFinderScript finder;
+
+	// quest manager
+	public QuestManager manager;
+
+	// quest object
+	public QuestObject objeto;
+
+	// stuff for words
+	public string partsOfSpeech = "speech.txt"; 		// public var for the file to read/write
+	public List<string> verbs = new List<string>(); 	// list of verbs
+	public List<string> nouns = new List<string>();		// list of nouns
+	const char DELIMITER = '|';
+
+	// Use this for initialization
+	void Start () {
+
+		// get scripts
+		finder = this.gameObject.GetComponent<QuestFinderScript> ();
+		manager = this.gameObject.GetComponent<QuestManager> ();
+
+
+	// *********** STREAM READER/STREAM WRITER ***********
+		// print dataPath
+		Debug.Log ("Path: " + Application.dataPath);
+
+		// create a string variable to the path of the file we want to use
+		string finalFilePath = Application.dataPath + "/" + partsOfSpeech;
+
+		// make a streamwriter and then write to it
+		// it will be dependent on the length of the verbs string, so we should take note of
+		// that and make the verbs and nouns lists the same length because i don't know what
+		// would happen otherwise. the "true" means it constantly adds to one file -- which
+		// might be cool for having carryover in multiple play sessions. Otherwise, make false.
+		StreamWriter sw = new StreamWriter(finalFilePath, true);
+
+		for (int i = 0; i < verbs.Count; i++) {
+			sw.WriteLine (verbs[i] + "|" + nouns[i]);
+		}
+
+		sw.Close ();
+
+		// read from the file that exists. In the near future, I will use this to at least pop-
+		// ulate to the quest manager, even if it's not functional, but this is a good skeleton.
+		// will also have to make this work looking for objects that have can be used for quests
+		// true as well as with the quest object script
+		StreamReader sr = new StreamReader(finalFilePath);
+
+		while (!sr.EndOfStream){
+			// read a line
+			string line = sr.ReadLine ();
+
+			// split line based on what's before the split and what's after
+			string[] splitLine = line.Split(DELIMITER);
+
+			// get the value out of the first part of the array
+			string verb = splitLine[0];
+
+			// get the second part of the array
+			string noun = splitLine[1];
+		}
+
+		sr.Close ();
+	// *********** END STREAMS ***********
+
+		// FIND EACH GAME OBJECT IN INTERACTABLES AND ADD ONE QUEST ID FOR EACH ITEM
+		// this is where I randomize based on the streamreader whoooooa
+//		foreach (GameObject go in finder.interactables) {
+//			manager.questList.
+//		}
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
--- Assets/Jenna/Scripts/QuestFinderScript.cs
+++ Assets/Jenna/Scripts/QuestFinderScript.cs
@@ -9,57 +9,74 @@ using UnityEngine;
 
 // but why or how is that different than a for loop?
 
-public class QuestFinderScript : MonoBehaviour {
+// note to self: make a function to check automatically if something ain't on a list
+// and if it ain't, add it, else don't
+
+public class QuestFinderScript : MonoBehaviour
+{
 	GameObject[] objects;
+	// for seeing if they're interactable
 	public List<GameObject> interactables = new List<GameObject> ();
 	int interactablesSize;
 	int objectsSize;
 
-//	public InteractionSettings interactionSettings;
+	// for seeing if they have QuestObject script
+	public List<GameObject> hasObjectScript = new List<GameObject> ();
 
-	void Start(){
+	void Start ()
+	{
 
 		// find the items in the scene and add them to a list of questable items
 		interactables = new List<GameObject> ();
-		objects = GameObject.FindObjectsOfType<GameObject>();
+		objects = GameObject.FindObjectsOfType<GameObject> ();
 		objectsSize = objects.Length;
 
 		// loop through the items in the scene
 		for (int i = 0; i < objectsSize; i++) {
 			InteractionSettings iset = objects [i].GetComponentInChildren<InteractionSettings> ();
-				if (iset != null) {
-				interactables.Add (iset.gameObject);
-
+			if (iset != null) {
+				GameObject iset1 = iset.gameObject.transform.parent.gameObject;
+				//interactables.Add (iset.gameObject);
+				if (!interactables.Contains (iset1)) {
+					interactables.Add (iset1);
+				}
 				// all right, so if it's on the list, calculate a random float
 				// to determine whether or not it can be used for quests
-				if (interactables.Contains (iset.gameObject)) {
+				if (interactables.Contains (iset1)) {
 					float ranger = Random.Range (0f, 1f);
 					if (ranger > 0.5f) {
 						iset.canBeUsedForQuests = true;
 					} else {
 						iset.canBeUsedForQuests = false;
 					}
-				}
-			}
-		
-			foreach (GameObject go in interactables){
 
-				if (iset.canBeUsedForQuests == true) {
-					Debug.Log (go + "can be used for quests");
-					// I think the generator will have to be a get/set maybe, or a function,
-					// not really sure, but it'll go here
-					// it's hard because, like, the iset had to be nested in the for loop because reasons
-					// so it'll be hard to access outside of this function.
-					// I guess it'll all go in start for now, it's not like it can go anywhere else
-					// might make load times kind of slow, but it'll be done before the player is there
-					// what will be difficult, however, is making it reactive when new objects are added
-					// but that's something I/we can handle later.
+					foreach (GameObject go in interactables) {
+						if (iset.canBeUsedForQuests == true) {
+						
+							//add quest object script
+							QuestObject quo = go.GetComponent<QuestObject> ();
+							if (quo != null) {
+								if (!hasObjectScript.Contains (go)) {
+									hasObjectScript.Add (go);
+								}
+							} else {
+								go.gameObject.AddComponent<QuestObject> ();
+								if (!hasObjectScript.Contains (go)) {
+									hasObjectScript.Add (go);
+								}
+							}
+						}
+					}
+				}
+						// I think the generator will have to be a get/set maybe, or a function,
+						// not really sure, but it'll go here
+						// it's hard because, like, the iset had to be nested in the for loop because reasons
+						// so it'll be hard to access outside of this function.
+						// I guess it'll all go in start for now, it's not like it can go anywhere else
+						// might make load times kind of slow, but it'll be done before the player is there
+						// what will be difficult, however, is making it reactive when new objects are added
+						// but that's something I/we can handle later.
 				}
 			}
 		}
 	}
-
-	public void Generator(GameObject quester, QuestObject quo, int questID){
-		
-	}
-}
