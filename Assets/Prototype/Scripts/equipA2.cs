d6ca07972d1a27154ffc62cfc76f8b428bfd9efc
010011100010 <ll3305@nyu.edu>
2017-03-07 23:53:00 -0500

    Working on a new equipping system

---
+++ Assets/Prototype/Scripts/equipA2.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class equipA2 : MonoBehaviour {
+    public GameObject equippable;
+    public GameObject equipPrompt; //this is the UI text displayed when player enters the trigger
+    public Collider equipCol; //drag the child box trigger collider of an equippable item here
+    public Vector3 cameraPos;
+    public KeyCode equipKey = KeyCode.E;
+    public KeyCode abandonKey = KeyCode.G;
+    private bool activatePrompt = false;
+
+    // Use this for initialization
+    void Start () {
+        equipPrompt.SetActive(false);
+
+    }
+
+    void OnTriggerExit(Collider equipCol)
+    {
+        equipPrompt.SetActive(false);
+    }
+
+    void OnTriggerEnter(Collider equipCol)
+    {
+        equipPrompt.SetActive(true);
+        if (Input.GetKeyDown(equipKey))
+        {
+            activatePrompt = true;
+        }
+    }
+
+    // Update is called once per frame
+    void Update () {
+        if (activatePrompt && Input.GetKeyDown(equipKey))
+        {
+            equipPrompt.SetActive(true);
+        }
+    }
+
+    void MoveToCamera ()
+    {
+
+
+    }
+
+    
+}
