25750269a94d79a12fc7b21bdc1e864dd92f67de
010011100010 <ll3305@nyu.edu>
2017-03-08 21:30:17 -0500

    The new equip funtion now somewhat works

--- Assets/Prototype/Scripts/equipA2.cs
+++ Assets/Prototype/Scripts/equipA2.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class equipA2 : MonoBehaviour {
+
     public GameObject equippable;
     public GameObject equipPrompt; //this is the UI text displayed when player enters the trigger
     public Collider equipCol; //drag the child box trigger collider of an equippable item here
@@ -10,6 +11,8 @@ public class equipA2 : MonoBehaviour {
     public KeyCode equipKey = KeyCode.E;
     public KeyCode abandonKey = KeyCode.G;
     private bool activatePrompt = false;
+    private GameObject player;
+    public float speed = 1;
 
     // Use this for initialization
     void Start () {
@@ -37,12 +40,19 @@ public class equipA2 : MonoBehaviour {
         {
             equipPrompt.SetActive(true);
         }
+
+        if (Input.GetKeyDown(equipKey))
+        {
+            MoveToCamera();
+        }
     }
 
     void MoveToCamera ()
     {
-
-
+        player = GameObject.Find("FPSController");
+        float step = speed * Time.deltaTime;
+        transform.position = Vector3.MoveTowards(transform.position, player.transform.position, step);
+        Debug.Log("why");
     }
 
     
