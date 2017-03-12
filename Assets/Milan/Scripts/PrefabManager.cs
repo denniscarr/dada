00bd68de6043711e6fb71ef7b0bc217b9b44d010
milank-s <mks554@nyu.edu>
2017-03-11 19:46:53 -0500

    map mesh height added

--- Assets/Milan/Scripts/PrefabManager.cs
+++ Assets/Milan/Scripts/PrefabManager.cs
@@ -11,35 +11,10 @@ public class PrefabManager : MonoBehaviour {
 	public GameObject[] LEVELPREFABS;
 	public GameObject[] NPCPREFABS;
 
-	private Sprite[] _sprites;
+	public Sprite[] _sprites;
 
 	void Awake(){
 		_sprites = Resources.LoadAll<Sprite> ("");
 		NPCPREFABS = Resources.LoadAll<GameObject> ("ObjectPrefabs");
-	}
-
-	public GameObject ObjectFactory(int x){
-
-		GameObject newObject = null;
-
-		switch (x) {
-
-		case 8:
-			newObject = Instantiate (Services.Prefabs.LEVELPREFABS[3], Vector3.zero, Quaternion.identity) as GameObject;
-			break;
-		case 7:
-			newObject = Instantiate (Services.Prefabs.LEVELPREFABS[2], Vector3.zero, Quaternion.identity) as GameObject;
-			break;
-		case 3:
-			newObject = Instantiate (Services.Prefabs.LEVELPREFABS[1], Vector3.zero, Quaternion.identity) as GameObject;
-			break;
-		default:
-			return null;
-			break;
-		}
-
-		newObject.GetComponent<Renderer> ().material.mainTexture = _sprites [Random.Range (0, _sprites.Length)].texture;
-
-		return newObject;
 	}
 }
