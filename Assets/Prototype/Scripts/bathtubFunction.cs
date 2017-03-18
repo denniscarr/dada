f9338a38004c55ec50db335b0ebafd0a32d4053d
010011100010 <ll3305@nyu.edu>
2017-03-18 15:03:41 -0400

    Functional update #3
    Null reference error fixed;
    drawer spray steam now.

---
+++ Assets/Prototype/Scripts/bathtubFunction.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class bathtubFunction : MonoBehaviour {
+	public GameObject bathtub;
+	public float bathtubForce = 500f;
+	public float bathtubSpeed = 1000f;
+	public KeyCode useBathtub = KeyCode.Mouse0;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (bathtub.transform.parent != null && Input.GetKeyDown (useBathtub)) {
+			GetComponent<Collider>().enabled = true;
+			GetComponent<Rigidbody>().useGravity = true;
+			GetComponent<Rigidbody>().AddForce(transform.right * bathtubSpeed);
+			transform.SetParent(null);
+		}
+	}
+}
