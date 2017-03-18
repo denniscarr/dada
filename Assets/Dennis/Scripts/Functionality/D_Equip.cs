d5408f8e34704b7133ea26dc449400500f1260fb
denniscarr <dcarr1@gmail.com>
2017-03-18 19:45:50 -0400

    Use Stuff

---
+++ Assets/Dennis/Scripts/Functionality/D_Equip.cs
@@ -0,0 +1,89 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class D_Equip : MonoBehaviour {
+
+    Writer writer;
+
+    public KeyCode equipKey = KeyCode.E;
+    public KeyCode abandonKey = KeyCode.G;
+	private bool readyToEquip = false;
+    Transform equipReference;
+
+    InteractionSettings intSet;
+
+    // Related to the throw force.
+    public float speed = 1;
+	public float ASpeed = 10;
+
+    Vector3 originalScale;
+
+
+    void Start ()
+    {
+        // Get references to my buddies.
+        writer = GetComponent<Writer>();
+        equipReference = GameObject.Find("Equip Reference").transform;
+        intSet = transform.parent.GetComponentInChildren<InteractionSettings>();
+        originalScale = transform.parent.localScale;
+    }
+
+
+    void OnTriggerExit(Collider collider)
+    {
+		Debug.Log ("Trigger exit");
+		readyToEquip = false;
+    }
+
+    void OnTriggerEnter(Collider collider)
+    {
+		if (collider.gameObject.name == "Player") {
+			readyToEquip = true;
+		}
+
+        writer.WriteSpecifiedString("Press E to equip " + transform.parent.name + ".");
+    }
+
+    // Update is called once per frame
+    void Update ()
+    {
+        if (readyToEquip == true && Input.GetKeyDown(equipKey))
+        {
+            MoveToCamera();
+			intSet.carryingObject = GameObject.Find("Player").transform;
+        }
+
+		if (Input.GetKeyDown (abandonKey)) {
+			abandonItem ();
+            intSet.carryingObject = null;
+		}
+    }
+
+    void MoveToCamera ()
+    {
+        // Disable collision & gravity.
+		GetComponentInParent<Collider>().enabled = false;
+        GetComponent<Collider>().enabled = false;
+		GetComponentInParent<Rigidbody>().isKinematic = true;
+
+        // Set position & parentage.
+		transform.parent.position = equipReference.position;
+		transform.parent.rotation = equipReference.rotation;
+		transform.parent.SetParent(equipReference, true);
+    }
+
+	void abandonItem ()
+	{
+        transform.parent.SetParent(null);
+
+        // Re-enable collision & stuff.
+        GetComponentInParent<Collider>().enabled = true;
+        GetComponent<Collider>().enabled = true;
+		GetComponentInParent<Rigidbody>().isKinematic = false;
+        transform.parent.localScale = originalScale;
+
+		GetComponentInParent<Rigidbody>().AddForce(transform.forward * ASpeed);
+	}
+    
+}
