228a8e99013195c4370dc4131ae86d8ffe6b5359
010011100010 <ll3305@nyu.edu>
2017-03-21 17:01:40 -0400

    More functions are in prefab

---
+++ Assets/Dennis/Scripts/Functionality/D_bathtubFunction.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class D_bathtubFunction : D_Function {
+	public float bathtubForce = 500f;
+	public float bathtubSpeed = 1000f;
+	// Use this for initialization
+	new void Start () {
+		base.Start();
+	}
+	
+	// Update is called once per frame
+	public override void Use () {
+		base.Use();
+		while (transform.parent.GetComponent<Rigidbody> ().isKinematic == true) {
+			transform.parent.SetParent (null);
+			transform.parent.GetComponentInChildren<D_Equip> ().abandonItem ();
+		}
+		GetComponentInParent<Rigidbody>().AddForce(transform.right * bathtubSpeed);
+		Invoke ("vibrate", 3f);
+	}
+
+	void vibrate() {
+		GetComponentInParent<Rigidbody> ().AddForce (transform.up * bathtubForce);
+		Invoke ("vibrate2", 0.3f);
+	}
+
+	void vibrate2() {
+		GetComponentInParent<Rigidbody> ().AddForce (-transform.right * bathtubForce);
+		Invoke ("vibrate3", 0.3f);
+	}
+
+	void vibrate3() {
+		GetComponentInParent<Rigidbody> ().AddForce (-transform.forward * bathtubForce);
+	}
+}
