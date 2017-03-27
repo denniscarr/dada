ee22d3efcce4742a65267db3fdfe6a4123616e70
010011100010 <ll3305@nyu.edu>
2017-03-27 16:22:19 -0400

    More "Resources" functionalities change
    Small table rotates;
    table randomly spawn an object;
    peacock makes a squaking noise

--- Assets/ProofOfConcept/Scripts/Functionality/D_microwaveFunction.cs
+++ Assets/ProofOfConcept/Scripts/Functionality/D_microwaveFunction.cs
@@ -3,16 +3,22 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class D_microwaveFunction : D_Function {
-	public GameObject ringWave;
-	public Transform emitter;
+	//public GameObject ringWave;
+	private ParticleSystem ring;
+	//public Transform emitter;
 	// Use this for initialization
 	new void Start () {
 		base.Start ();
+		ring = GetComponent<ParticleSystem> ();
+		var em = ring.emission;
+		em.enabled = false;
 	}
 	
 	// Update is called once per frame
 	public override void Use () {
 		base.Use ();
-		Instantiate (ringWave, emitter.transform.right, Quaternion.identity);
+		//Instantiate (ringWave, emitter.transform.right, Quaternion.identity);
+		var em = ring.emission;
+		em.enabled = !em.enabled;
 	}
 }
---
+++ Assets/ProofOfConcept/Scripts/Functionality/D_peacockFunction.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class D_peacockFunction : D_Function {
+	public AudioSource peacockSource;
+	public AudioClip peacockClip;
+	// Use this for initialization
+	new void Start () {
+		base.Start ();
+	}
+	
+	// Update is called once per frame
+	public override void Use () {
+		base.Use ();
+		peacockSource.PlayOneShot (peacockClip);
+	}
+}
