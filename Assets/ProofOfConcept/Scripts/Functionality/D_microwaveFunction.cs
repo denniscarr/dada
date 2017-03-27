faf6755c85481b466c3f3d29fa3538f577dfa44c
010011100010 <ll3305@nyu.edu>
2017-03-27 14:41:14 -0400

    Another "Resource" funtionality change
    Added ring particle particle sprite and material;
    the microwave emits ring when used.

---
+++ Assets/ProofOfConcept/Scripts/Functionality/D_microwaveFunction.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class D_microwaveFunction : D_Function {
+	public GameObject ringWave;
+	public Transform emitter;
+	// Use this for initialization
+	new void Start () {
+		base.Start ();
+	}
+	
+	// Update is called once per frame
+	public override void Use () {
+		base.Use ();
+		Instantiate (ringWave, emitter.transform.right, Quaternion.identity);
+	}
+}
