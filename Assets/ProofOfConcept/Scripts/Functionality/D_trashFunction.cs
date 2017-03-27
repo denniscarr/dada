66ddb5a7b6f3b3b2d5caf788e008da01a684d2bb
010011100010 <ll3305@nyu.edu>
2017-03-27 17:38:53 -0400

    More functions
    Player drinks from the toilet;
    bed spawns a NPC;
    trash can shoots confetti.

---
+++ Assets/ProofOfConcept/Scripts/Functionality/D_trashFunction.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class D_trashFunction : D_Function {
+	public GameObject confetti;
+	public AudioClip confettiClip;
+	private AudioSource confettiSource;
+	// Use this for initialization
+	new void Start () {
+		base.Start ();
+		confettiSource = GetComponent<AudioSource> ();
+	}
+	
+	// Update is called once per frame
+	public override void Use () {
+		base.Use ();
+		Instantiate (confetti, transform.position, Quaternion.identity);
+		confettiSource.PlayOneShot (confettiClip);
+	}
+}
