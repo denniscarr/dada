600c3856613eed1e9e1d93cd24fb6620d394466b
010011100010 <ll3305@nyu.edu>
2017-04-01 14:22:42 -0400

    Added the hammer and animation

---
+++ Assets/ProofOfConcept/Scripts/Functionality/D_hammerFunction.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class D_hammerFunction : D_Function {
+	Animator hammer;
+	// Use this for initialization
+	new void Start () {
+		base.Start ();
+		hammer = GetComponentInParent<Animator> ();
+	}
+	
+	// Update is called once per frame
+	public override void Use () {
+		base.Use ();
+		//GetComponentInParent<Animation> ().Play();
+		hammer.SetBool("Hammer", true);
+		print ("animation triggered");
+
+	}
+}
