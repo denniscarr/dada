45de490eb2756c79cd59ed46ddbbdf31bdf9eb6e
010011100010 <ll3305@nyu.edu>
2017-04-11 17:45:07 -0400

    Still working on the hammer function

--- Assets/ProofOfConcept/Scripts/Functionality/D_hammerFunction.cs
+++ Assets/ProofOfConcept/Scripts/Functionality/D_hammerFunction.cs
@@ -3,18 +3,18 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class D_hammerFunction : D_Function {
-	Animator hammer;
+	//Animator hammer;
 	// Use this for initialization
 	new void Start () {
 		base.Start ();
-		hammer = GetComponentInParent<Animator> ();
+		//hammer = GetComponentInParent<Animator> ();
 	}
 	
 	// Update is called once per frame
 	public override void Use () {
 		base.Use ();
 		//GetComponentInParent<Animation> ().Play();
-		hammer.SetBool("Hammer", true);
+		//hammer.SetBool("Hammer", true);
 		print ("animation triggered");
 
 	}
