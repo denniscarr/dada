dbf3d9120fcd237ed77c3eade6ce2fbc7a5320ae
milank-s <mks554@nyu.edu>
2017-04-11 21:01:59 -0400

    sprite changed, sky removed
    totems spawn on hills, sprites change when clicked, sprites face the normals they're spawned on.

--- Assets/Milan/Scripts/LevelManager.cs
+++ Assets/Milan/Scripts/LevelManager.cs
@@ -6,7 +6,7 @@ using UnityEngine.SceneManagement;
 public class LevelManager : SimpleManager.Manager<Level> {
 
 	public Services.TYPES[] props;
-
+	public bool showSky = false;
 	public GameObject SceneText;
 	public Level currentLevel;
 	public Light cookieLight;
@@ -34,6 +34,8 @@ public class LevelManager : SimpleManager.Manager<Level> {
 		Level.xOrigin = Random.Range (0, 10000);
 		Level.yOrigin = Random.Range (0, 10000);
 		Level.noiseScale = perlinFrequency;
+
+		RenderSettings.fogEndDistance = 100;
 
         Create();
 
