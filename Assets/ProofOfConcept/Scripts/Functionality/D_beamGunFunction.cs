08f3817fcdbf833107675dbddc2121a09e36daed
010011100010 <ll3305@nyu.edu>
2017-04-15 16:32:32 -0400

    We have a beam pistol now

---
+++ Assets/ProofOfConcept/Scripts/Functionality/D_beamGunFunction.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class D_beamGunFunction : D_Function {
+	LineRenderer line;
+	// Use this for initialization
+	new void Start () {
+		base.Start ();
+		line = gameObject.GetComponent<LineRenderer> ();
+		line.enabled = false;
+	}
+	
+	// Update is called once per frame
+	public override void Use () {
+		base.Use();
+		line.enabled = true;
+		Ray beamRay = new Ray (transform.position, -transform.forward);
+		RaycastHit Hit;
+		line.SetPosition (0, beamRay.origin);
+		if (Physics.Raycast(beamRay, out Hit, 100))
+			line.SetPosition(1, Hit.point);
+		else line.SetPosition (1, beamRay.GetPoint (100));
+
+		Invoke ("BeamOff", 0.1f);
+	}
+
+	public void BeamOff() {
+		line.enabled = false;
+	}
+	/*IEnumerator FireLaser(){
+		line.enabled = true;
+	}*/
+}
