7d94d966f6ca0a967704d06a502de2dbdb5ea6cf
denniscarr <dcarr1@gmail.com>
2017-04-14 21:13:23 -0400

    Not really sure what I did, everything's a blur.

---
+++ Assets/Prototype/Scripts/Editor/InteractionSettingsEditor.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(InteractionSettings))]
+public class InteractionSettingsEditor : Editor {
+
+    public override void OnInspectorGUI()
+    {
+        DrawDefaultInspector();
+
+        InteractionSettings myTarget = (InteractionSettings)target;
+
+        if (GUILayout.Button("Print ableToBeCarried"))
+        {
+            Debug.Log(myTarget.name + " able to be carried: " + myTarget.ableToBeCarried);
+        }
+    }
+
+}
