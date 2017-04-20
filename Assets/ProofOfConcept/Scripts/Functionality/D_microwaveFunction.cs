44ec11a151411407f3c2fd44cc9872503371508a
denniscarr <dcarr1@gmail.com>
2017-04-20 15:17:06 -0400

    Added a new cursor sprite (not hooked up in code yet)

--- Assets/ProofOfConcept/Scripts/Functionality/D_microwaveFunction.cs
+++ Assets/ProofOfConcept/Scripts/Functionality/D_microwaveFunction.cs
@@ -22,12 +22,9 @@ public class D_microwaveFunction : D_Function {
 
         if (em.enabled)
         {
-//<<<<<<< HEAD
 			Vector3 parentPos = LOWER_EQUIP_REFERENCE.position + intSet.equipPosition;
 			RaycastHit[] hits = Physics.SphereCastAll(parentPos, 2f, t_player.right, 5f);
-//=======
 //            RaycastHit[] hits = Physics.SphereCastAll(transform.parent.position, 2f, transform.parent.right, 13f);
-//>>>>>>> master
             //Debug.DrawRay(transform.parent.position, transform.parent.right * 2f, Color.cyan);
             foreach (RaycastHit hit in hits)
             {
