23079a0d7e176c69be007723dfc740bff51a664a
010011100010 <ll3305@nyu.edu>
2017-04-23 23:31:19 -0400

    Molecular generator is working

--- Assets/ProofOfConcept/Scripts/Functionality/D_vanillaGunFunction.cs
+++ Assets/ProofOfConcept/Scripts/Functionality/D_vanillaGunFunction.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class D_vanillaGunFunction : D_Function {
 	private LineRenderer line;
 	public GameObject[] atoms;
+    public GameObject[] molecules;
 	public int atomsCount = 50;
 	// Use this for initialization
 	new void Start () {
@@ -25,16 +26,25 @@ public class D_vanillaGunFunction : D_Function {
 		line.SetPosition (0, beamRay.origin);
 
 		if (Physics.Raycast (beamRay, out Hit, 100)) {
-			line.SetPosition (1, Hit.point);
-			GameObject atomicHit = Hit.collider.gameObject;
-			Vector3 atomicBox = atomicHit.transform.localScale;
-			Vector3 atomicPos = new Vector3 (Random.value * atomicBox.x, Random.value * atomicBox.y, Random.value * atomicBox.z);
-			atomicPos = transform.TransformPoint (atomicBox * 0.5f);
-			Destroy (atomicHit);
-			for (int i = 0; i < atomsCount; i++) {
-				Instantiate (atoms [Random.Range (0, atoms.Length - 1)], atomicPos, Quaternion.identity);
-			}
-		} else {
+            GameObject atomicHit = Hit.collider.gameObject;
+            if (atomicHit.name.Contains("atom"))
+            {
+                print("Atom cannot be reduced");
+            }
+            else
+            {
+                Vector3 atomicBox = atomicHit.transform.localScale;
+                Vector3 atomicPos = new Vector3(Random.value * atomicBox.x, Random.value * atomicBox.y, Random.value * atomicBox.z);
+                atomicPos = transform.TransformPoint(atomicBox * 0.5f);
+                Destroy(atomicHit);
+                for (int i = 0; i < atomsCount; i++)
+                {
+                    Instantiate(atoms[Random.Range(0, atoms.Length - 1)], atomicPos, Quaternion.identity);
+                    Instantiate(molecules[Random.Range(0, molecules.Length - 1)], atomicPos, Quaternion.identity);
+                }
+            }
+            line.SetPosition(1, Hit.point);
+        } else {
 			line.SetPosition(1, beamRay.GetPoint(100));	
 		}
 		Invoke ("BeamOff", 0.1f);
