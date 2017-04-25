874f3d4655fa7bfbae90e6ebae51f58e71d4dbc7
denniscarr <dcarr1@gmail.com>
2017-04-24 20:09:36 -0400

    Started adding more money things.

---
+++ Assets/MoneyFunction.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoneyFunction : D_Function {
+
+    int value;
+
+
+    new private void Start()
+    {
+        base.Start();
+
+        // Name game.
+        value = Random.Range(50, 100);
+        transform.parent.name = "$" + value;
+
+        intSet.isOwnedByPlayer = true;
+    }
+
+
+    public override void Use()
+    {
+        base.Use();
+
+        if (intSet.carryingObject.name == "Player")
+        {
+            // Play cha-ching sound....
+            Debug.Log("cha ching");
+
+            // Give player money
+            GameObject.Find("Bootstrapper").GetComponent<PlayerMoneyManager>().funds += value;
+        }
+
+        Destroy(transform.parent.gameObject);
+    }
+}
