a6de04dabe9d62e11a11ad6b253a2a125cdaec26
Vanilla <swingliu0831@foxmail.com>
2017-04-25 16:05:19 -0400

    fix atom generator

--- Assets/Longxiao/Atom prefab & material/triagonalPlanarMolecule.cs
+++ Assets/Longxiao/Atom prefab & material/triagonalPlanarMolecule.cs
@@ -13,7 +13,7 @@ public class triagonalPlanarMolecule : MonoBehaviour {
 
 		//Spawn bounding keys around the center atom
 		key = GameObject.Find ("Linear molecular generator").GetComponent<linearMolecule> ().key;
-		Vector3 key1 = new Vector3((centerAtom.transform.position.x + (centerAtom.transform.localScale.x) / 2)+0.4f, centerAtom.transform.position.y, centerAtom.transform.position.z);
+		Vector3 key1 = centerAtom.transform.position + new Vector3(((centerAtom.transform.localScale.x) / 2)+0.4f, 0, 0);
 		GameObject boundingKey1 = Instantiate (key, key1, Quaternion.Euler(0,0,90));
 		GameObject boundingKey2 = Instantiate (key, key1, Quaternion.Euler(0,0,90));
 		GameObject boundingKey3 = Instantiate (key, key1, Quaternion.Euler(0,0,90));
@@ -24,19 +24,19 @@ public class triagonalPlanarMolecule : MonoBehaviour {
 		boundingKey3.transform.RotateAround (centerAtom.transform.position, centerAtom.transform.up, 240);
 
 		//Spawn surrounding atoms
-		int num = Random.Range (0, atom.Length - 1);
-		GameObject sideAtom1 = Instantiate (atom[num], key1, Quaternion.identity);
-		GameObject sideAtom2 = Instantiate (atom[num], boundingKey2.transform.position, Quaternion.identity);
-		GameObject sideAtom3 = Instantiate (atom[num], boundingKey3.transform.position, Quaternion.identity);
+		//int num = Random.Range (0, atom.Length - 1);
+		GameObject sideAtom1 = Instantiate (atom[Random.Range (0, atom.Length - 1)]);
+		GameObject sideAtom2 = Instantiate (atom[Random.Range (0, atom.Length - 1)]);
+		GameObject sideAtom3 = Instantiate (atom[Random.Range (0, atom.Length - 1)]);
 		Destroy(sideAtom1.GetComponent<Rigidbody>());
 		Destroy(sideAtom2.GetComponent<Rigidbody>());
 		Destroy(sideAtom3.GetComponent<Rigidbody>());
-		sideAtom1.transform.position =new Vector3 (key1.x + sideAtom1.transform.localScale.x/2, key1.y, key1.z);
-		sideAtom2.transform.position =new Vector3 (boundingKey2.transform.position.x - sideAtom2.transform.localScale.x/2, boundingKey2.transform.position.y, boundingKey2.transform.position.z - sideAtom2.transform.localScale.z/2);
-		sideAtom3.transform.position = new Vector3 (boundingKey3.transform.position.x - sideAtom3.transform.localScale.x / 2, boundingKey3.transform.position.y, boundingKey3.transform.position.z + sideAtom3.transform.localScale.z / 2);
-		sideAtom1.transform.parent = centerAtom.transform;
-		sideAtom2.transform.parent = centerAtom.transform;
-		sideAtom3.transform.parent = centerAtom.transform;
+		sideAtom1.transform.position = key1 + new Vector3 (sideAtom1.transform.localScale.x/2, 0, 0);
+		sideAtom2.transform.position = boundingKey2.transform.position + -boundingKey2.transform.up*sideAtom2.transform.localScale.x/2f;//new Vector3 ( - sideAtom2.transform.localScale.x/2, 0, -sideAtom2.transform.localScale.z / 2);
+		sideAtom3.transform.position = boundingKey3.transform.position + -boundingKey3.transform.up*sideAtom3.transform.localScale.x/2f;// new Vector3 ( - sideAtom3.transform.localScale.x/2, 0, sideAtom3.transform.localScale.z / 2);
+		sideAtom1.transform.SetParent(centerAtom.transform, true);// centerAtom.transform;
+		sideAtom2.transform.SetParent(centerAtom.transform, true);//parent = centerAtom.transform;
+		sideAtom3.transform.SetParent(centerAtom.transform, true);//parent = centerAtom.transform;
 	}
 	
 	// Update is called once per frame
