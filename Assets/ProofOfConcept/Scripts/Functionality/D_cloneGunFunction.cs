887ad3ffd3583dfeb586a5da79b2b646a8a75314
010011100010 <ll3305@nyu.edu>
2017-04-29 17:45:28 -0400

    Funtion updates
    Added a clone gun that will clone whatever shot by it;
    mirror desk randomly pull an item towards player now;
    working on an item that will assign soul to any item hit by it.

---
+++ Assets/ProofOfConcept/Scripts/Functionality/D_cloneGunFunction.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class D_cloneGunFunction : D_Function {
+	private LineRenderer line;
+	// Use this for initialization
+	new void Start () {
+		base.Start ();
+		line = gameObject.GetComponent<LineRenderer> ();
+		line.enabled = false;
+	}
+	
+	// Update is called once per frame
+	public override void Use () {
+		base.Use ();
+		line.enabled = true;
+
+		Ray cloneRay = new Ray (transform.position, transform.forward);
+		RaycastHit hit;
+
+		line.SetPosition (0, cloneRay.origin);
+
+		if (Physics.Raycast (cloneRay, out hit, 100)) {
+			GameObject cloneHit = hit.collider.gameObject;
+			Instantiate (cloneHit, hit.transform.position, Quaternion.identity);
+			line.SetPosition (1, hit.point);
+		} else {
+			line.SetPosition (1, cloneRay.GetPoint (100));
+		}
+
+		Invoke ("BeamOff", 0.1f);
+	}
+
+	 void BeamOff() {
+		line.enabled = false;
+	}
+}
