fc0db29dd2329e9d45c582f717103a0177e07776
010011100010 <ll3305@nyu.edu>
2017-04-29 19:58:40 -0400

    Gohei of reverse exorcism is working. It turns items into NPCs.

--- Assets/Longxiao/soulParticle.cs
+++ Assets/Longxiao/soulParticle.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class soulParticle : MonoBehaviour {
 	private ParticleSystem particleOfSoul;
 	private List<ParticleCollisionEvent> collisionEvents;
+	public GameObject soul;
 	// Use this for initialization
 	void Start () {
 		particleOfSoul = GetComponent<ParticleSystem> ();
@@ -17,10 +18,15 @@ public class soulParticle : MonoBehaviour {
 	}
 
 	void OnParticleCollision (GameObject futureNPC) {
-		int numCollisionEvents = particleOfSoul.GetCollisionEvents (futureNPC, collisionEvents);
-		int i = 0;
-		while (i < numCollisionEvents) {
-			print (futureNPC.name);
+		//int numCollisionEvents = particleOfSoul.GetCollisionEvents (futureNPC, collisionEvents);
+		//int i = 0;
+		//while (i < numCollisionEvents) {
+		//	print (futureNPC.name);
+		//}
+		if (futureNPC.name != "GROUND" && futureNPC.GetComponentInChildren<NPC>() == null) {
+			print (futureNPC.name +" needs a soul");
+			Instantiate (soul, futureNPC.transform.position, Quaternion.identity, futureNPC.transform);
+//			soul.transform.parent = futureNPC.transform;
 		}
 
 	}
