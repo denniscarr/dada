0624d6cf7ce6005c80ea6a4182df5dd933b3e497
010011100010 <ll3305@nyu.edu>
2017-05-08 14:00:33 -0400

    Meteor collider fix. All meteors have proper colliders now. They will no longer clip through terrain and other objects. In addition, meteors will incinerate touching objects with grand explosions.

---
+++ Assets/Longxiao/meteorExplosion.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class meteorExplosion : MonoBehaviour {
+	public GameObject[] explosion;
+	private float explosionScale;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnCollisionEnter (Collision collision) {
+		explosionScale = Random.Range (10, 15);
+		GameObject impact = Instantiate (explosion [Random.Range (0, explosion.Length)], transform.position, Quaternion.identity) as GameObject;
+		//impact.transform.localScale = new Vector3 (explosionScale, explosionScale, explosionScale);
+		ParticleSystem impactParticle = impact.GetComponent<ParticleSystem>();
+		var impactPSMA = impactParticle.main;
+		//impactParticle.main.startSizeMultiplier = impactParticle.main.startSizeMultiplier * explosionScale;
+		impactPSMA.startSizeMultiplier = impactPSMA.startSizeMultiplier*explosionScale;
+		if (collision.gameObject.GetComponentInChildren<InteractionSettings>() != null)
+		{
+			collision.gameObject.GetComponentInChildren<InteractionSettings>().heat += 1f;
+		}
+	}
+
+}
