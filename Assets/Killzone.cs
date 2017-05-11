2ef64d6e6370d9a53082cf09888957b14617380f
denniscarr <dcarr1@gmail.com>
2017-05-11 16:59:06 -0400

    Grail no longer spawns a new level.

--- Assets/Killzone.cs
+++ Assets/Killzone.cs
@@ -6,15 +6,15 @@ public class Killzone : MonoBehaviour {
 
 	void OnTriggerEnter(Collider other)
     {
-        // If the grail has fallen off the level, spawn a new grail.
-        if (other.name == "Grail")
-        {
-            Services.LevelGen.Create();
-            other.GetComponent<Grail>().GetReadyToDie();
-        }
+        // If the grail has fallen off the level, spawn a new level.
+        //if (other.name == "Grail")
+        //{
+        //    Services.LevelGen.Create();
+        //    other.GetComponent<Grail>().GetReadyToDie();
+        //}
 
         // If this object is not the grail, put it back on the level.
-        else if ((other.GetComponentInChildren<InteractionSettings>() != null && !other.GetComponentInChildren<InteractionSettings>().IsEquipped) || other.gameObject == Services.Player)
+        if ((other.GetComponentInChildren<InteractionSettings>() != null && !other.GetComponentInChildren<InteractionSettings>().IsEquipped) || other.gameObject == Services.Player)
         {
             //Debug.Log("Killzone wrapped " + other.name);
             float currentLevelRadius = Services.LevelGen.currentLevel._width * Services.LevelGen.tileScale;
