98acac93e8db6ba0f68c0e154a5a53281fae319e
8ude <crbertelsen@gmail.com>
2017-05-13 19:32:05 -0400

    TV now changes music playback

--- Assets/ProofOfConcept/Scripts/Functionality/D_TVFunction.cs
+++ Assets/ProofOfConcept/Scripts/Functionality/D_TVFunction.cs
@@ -4,9 +4,11 @@ using UnityEngine;
 
 public class D_TVFunction : D_Function {
 	private float timeSpeed;
+	GameObject ambientMusic;
 	// Use this for initialization
 	new void Start () {
 		base.Start ();
+		ambientMusic = GameObject.Find ("MusicSources");
 		timeSpeed = Random.Range (0.1f, 10f);
 	}
 	
@@ -14,11 +16,21 @@ public class D_TVFunction : D_Function {
 	public override void Use () {
 		base.Use ();
 		Time.timeScale = timeSpeed;
+		foreach (AudioSource asource in ambientMusic.GetComponentsInChildren<AudioSource>()) {
+			if (asource.isPlaying) {
+				asource.pitch = timeSpeed;
+			}
+		}
         Invoke("BackToNormal", 60f);
 	}
 
     void BackToNormal()
     {
         Time.timeScale = 1f;
+		foreach (AudioSource asource in ambientMusic.GetComponentsInChildren<AudioSource>()) {
+			if (asource.isPlaying) {
+				asource.pitch = Time.timeScale;
+			}
+		}
     }
 }
