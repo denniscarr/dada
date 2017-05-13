07812bb47057d107bb77b193bbe688e935ecc948
010011100010 <ll3305@nyu.edu>
2017-05-13 19:28:28 -0400

    I have almost completed the NPC elimination quest. Now, one pickup quest and one elimination quest will be generated when player hit the ground. However, the quest item of both quests is the very same thing.

--- Assets/Jenna/Quests/NPCEliminationQuest.cs
+++ Assets/Jenna/Quests/NPCEliminationQuest.cs
@@ -2,15 +2,47 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NPCEliminationQuest : MonoBehaviour {
+public class NPCEliminationQuest : Quest {
 
 	// Use this for initialization
 	void Start () {
-		
+		base.Start ();
 	}
 	
 	// Update is called once per frame
-	void Update () {
-		
+	public void FixedUpdate () {
+		if (targetObject != null && targetObject.GetComponentInChildren<NPC> () != null && targetObject.GetComponentInChildren<NPC> ().died == true) {
+			FinishQuest();
+		}
+	}
+
+	public override void makeTheQuest(){
+		base.makeTheQuest ();
+		rewardMoney = Mathf.RoundToInt (Random.Range(1, 10000));
+		title = ("Eliminate the glowing " + targetObject.name + " ");
+		progress = Quest.QuestProgress.AVAILABLE;
+		description = (title + " by any means possible. Reward: $" + rewardMoney);
+		CopyComponent(this, targetObject);
+		spawnNote ();
+	}
+
+	// method to copy alla this shit on the pickupquest on the quest object generated
+	// in questbuilderscript
+	Component CopyComponent (Component original, GameObject destination)
+	{
+		System.Type type = original.GetType ();
+		Component copy = destination.AddComponent(type);
+
+		System.Reflection.FieldInfo[] fields = type.GetFields ();
+		foreach (System.Reflection.FieldInfo field in fields) {
+			field.SetValue (copy, field.GetValue(original));
+		}
+		return copy;
+	}
+
+	new void FinishQuest(){
+		if (completed) return;
+		base.FinishQuest ();
+
 	}
 }
--- Assets/Jenna/Scripts/QuestBuilderScript.cs
+++ Assets/Jenna/Scripts/QuestBuilderScript.cs
@@ -30,6 +30,9 @@ public class QuestBuilderScript : MonoBehaviour {
 	public int questThing, ranger, length;
 	public GameObject objeto;
 
+	//Store all game objects with NPC.cs attached in this list
+	private NPC[] enemies;
+
 
 	void Start () {
 
@@ -57,6 +60,7 @@ public class QuestBuilderScript : MonoBehaviour {
             if (manager.questsGeneratedInCurrentLevel < Services.Quests.questsToComplete)
             {
                 GeneratePickup();
+			GenerateElimination ();
 //                Debug.Log("Quests genereated in current level: " + manager.questsGeneratedInCurrentLevel);
 //                Debug.Log("Quests to complete: " + manager.questsToComplete);
 			}
@@ -109,6 +113,28 @@ public class QuestBuilderScript : MonoBehaviour {
     }
 
 	public void GenerateElimination(){
-		
+		if (Services.LevelGen.levelNum == 0) return;
+		enemies = GameObject.FindObjectsOfType<NPC> ();
+		GameObject enemy = enemies[Random.Range (0, enemies.Length)].gameObject;
+		if (enemy.GetComponent<NPCEliminationQuest> () != null)
+			return;
+		NPCEliminationQuest newQuest = enemy.AddComponent<NPCEliminationQuest> ();
+		if (enemy.GetComponents<NPCEliminationQuest> ().Length > 1) {
+			Destroy (enemy.GetComponents<NPCEliminationQuest> () [1]);
+			return;
+		}
+		this.gameObject.GetComponent<NPCEliminationQuest> ().makeTheQuest ();
+		manager.questList.Add (newQuest);
+
+		if (newQuest.progress == Quest.QuestProgress.AVAILABLE) {
+			manager.QuestRequest(objectScript);
+			//Debug.Log ("quest added to list");
+			this.gameObject.GetComponent<NPCEliminationQuest> ().spawnNote ();
+		}
+
+		if (newQuest.progress == Quest.QuestProgress.ACCEPTED) {
+			manager.currentQuestList.Add (newQuest);
+		}
+		manager.questsGeneratedInCurrentLevel++;
 	}
 }
