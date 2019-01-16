8fcf5f934a0e45afa11bfcee96ef1f9d55562972
denniscarr <dcarr1@gmail.com>
2019-01-16 01:48:32 -0500

    added a force crash script

---
+++ Assets/Dennis/ForceCrash.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ForceCrash : MonoBehaviour
+{
+    const float CRASHTIME = 15f;
+    const float crashFps = 1f;
+    float crashTimer = 0f;
+
+    private void Update() {
+        float fps = 1f / Time.deltaTime;
+        if (fps <= crashFps) {
+            crashTimer += Time.deltaTime;
+            if (crashTimer >= CRASHTIME) { Application.Quit(); }
+        } else {
+            crashTimer = 0f;
+        }
+    }
+}
